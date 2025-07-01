062dc44242c94ecea514e8adbc968a7fad5f22c8
Amanda Beatriz <amanda.rodrigues.jardim@escola.pr.gov.br>
2025-07-01 08:43:23 -0300

    Primeira versão do meu código

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'java Script',
+    'Para que serve?',
+    'O java serve para voce implementar itens complexos em pagínas web'
+)
+
+criaCartao(
+    'Figma',
+    'Qual a finalidade do figma?',
+    'Tornar os design acessível a todos'
+)
+
+criaCartao(
+    'Alura',
+    'Quem é o dono da Alura ?',
+    'Paulo Silvera é o CEO e confundador da Alura'
+)
+
+criaCartao(
+    'GitHub',
+    'Como o github pode ser definido?',
+    'É um projeto de código aberto de qualidade'
+)
